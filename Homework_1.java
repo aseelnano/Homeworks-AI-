c95c7fad49cc30922dcbf702c109360016ae3e9a
Aseel <as.nano2011@gmail.com>
2019-01-16 01:02:02 +0300

    v 0.1
    Make class and import Scanner

--- Homework_1.java
+++ Homework_1.java
@@ -1,1 +1,16 @@
+package homework_1;
 
+import java.util.Scanner;
+
+public class homework_1 {
+
+    
+    public static void main(String[] args) {
+       
+     Scanner x = new Scanner(System.in);
+     
+     int input = x.nextInt();
+     
+    }
+    
+}
